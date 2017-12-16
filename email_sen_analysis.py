ba3b52e9d0330606e17a591a148cb989db13ea36
TalhaGhaffar <t.ghaffar@yahoo.com>
2017-12-16 13:00:20 -0500

    email analysis

--- email_sen_analysis.py
+++ email_sen_analysis.py
@@ -4,19 +4,25 @@ import pandas as pd
 from  datetime import datetime
 import os
 import nltk
-
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 
-_dataset2_dir = "C:\\Users\\talha\\Documents\\DA\\da_project\\dataset2\\"
+_dataset2_dir = "C:/Users/talha/Documents/DA/da_project/dataset2/"
 email_file = _dataset2_dir + "email_info.csv"
 device_file = _dataset2_dir + "device_info.csv"
+file_info = _dataset2_dir + "file_info.csv"
 
 def get_polarity_and_subjectivity(text):
     blob = TextBlob(text)
     return pd.Series({"polarity": blob.sentiment.polarity, "subjectivity": blob.sentiment.subjectivity})
 
+def vader_sentiment_analysis(text):
+	 return analyzer.polarity_scores(text)['compound']
+
 chunk_size = 100000
-email_chunks = pd.read_csv(_dataset2_dir + "email_info.csv", index_col=0, chunksize=chunksize)
+email_chunks = pd.read_csv(file_info, index_col=0, chunksize=chunk_size)
+
+analyzer = SentimentIntensityAnalyzer()
 
 # breakAt = 3
 numChunks = 0
@@ -24,10 +30,10 @@ appended_data = []
 
 for chunk in email_chunks:
 	print ("\n ============ Processing chunk # "+ str(numChunks) +" =============\n")
-	pol_and_subj_df = chunk['content'].apply(get_polarity_and_subjectivity)
-	appended_data.append(pol_and_subj_df)
-	print (pol_and_subj_df.head())
-	print (pol_and_subj_df.shape)
+	sentiment = chunk['content'].apply(vader_sentiment_analysis)
+	appended_data.append(sentiment)
+	print (sentiment.head())
+	print (sentiment.shape)
 	numChunks+=1
 	# if numChunks == breakAt:
 	# 	break
@@ -37,19 +43,6 @@ appended_data = pd.concat(appended_data, axis=0)
 print (appended_data.head())
 print (appended_data.shape)
 print (type(appended_data))
-appended_data.to_pickle(_dataset2_dir+"email_sentiment_analysis_df")
+appended_data.to_pickle(_dataset2_dir+"file_info_vader_sentiment_analysis_df")
 
 print (numChunks)
-
-
-# print (type(email_df))
-# print (email_df.shape)
-# email_content = email_df['content']
-# pol_and_subj_df = email_df['content'].apply(get_polarity_and_subjectivity)
-
-# print (pol_and_subj_df.head())
-
-# device_chunks = pd.read_csv(device_file, index_col=0, chunksize=10000)
-
-# for device_chunk in device_chunks:
-# 	print (type(device_chunk))
